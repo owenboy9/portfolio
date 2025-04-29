82aba7654e95307784efbf658dee060beb5f98ef
owenboy9 <owenkraus9@gmail.com>
2025-04-29 10:28:24 +0200

    change basename

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,10 @@ import { Outlet } from 'react-router-dom';
 const Layout = () => {
   return (
     <div className="min-h-screen">
-      <Outlet />
+      {/* Add your Header and Footer components here later */}
+      <main>
+        <Outlet /> {/* This is where the content of each page will show */}
+      </main>
     </div>
   );
 };
--- src/pages/Home/Page.tsx
+++ src/pages/Home/Page.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <div>home</div>;
+  return (
+  <div>
+    <h1>Home</h1>
+    <p>Welcome to the home page!</p>
+    <p>This is a simple example of a React component.</p>
+    <p>You can add more content here as needed.</p>
+  </div>
+  );
 };
 
 export default Home;
